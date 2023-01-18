85966585b03546683095e26f2bec2e9d2196bc43
eremziom <eremziom@hotmail.com>
2023-01-18 11:18:54 +0100

    project init, router and i18n implememtation

---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    {{ $t('message') }}
+    <button @click="changeLang">change lang</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+  },
+  mounted() {
+    console.log(this.$i18n)
+  },
+  methods: {
+    changeLang(){
+      console.log(this.$i18n.locale)
+      this.$i18n.locale = 'en'
+    }
+  }
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+
+  pluginOptions: {
+    i18n: {
+      locale: 'pl',
+      fallbackLocale: 'pl',
+      localeDir: 'locales',
+      enableInSFC: true,
+      enableBridge: false
+    }
+  }
+})
